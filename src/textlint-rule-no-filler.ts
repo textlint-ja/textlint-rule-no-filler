b83a245f92b6500625801794adb348737475b8de
azu <azuciao@gmail.com>
2021-05-13 22:39:22 +0900

    fix

--- src/textlint-rule-no-filler.ts
+++ src/textlint-rule-no-filler.ts
@@ -1,20 +1,55 @@
-import { TextlintRuleReporter } from "@textlint/types";
+import type { TxtParentNode, TxtTextNode } from "@textlint/ast-node-types";
+import type { TextlintRuleReporter } from "@textlint/types";
+import { StringSource } from "textlint-util-to-string";
 import { tokenize } from "kuromojin";
 
 export type Options = {};
-const report: TextlintRuleReporter<Options> = (context) => {
-    const { Syntax, getSource, RuleError, report } = context;
+/**
+ * アルゴリズム
+ *
+ * - Paragraphから文を抽出する
+ * - Str nodeだけを見ると`A`と`B` のようなCodeに囲まれたものが、「と」となりフィラーと誤検知される
+ * - そのため、Paragraphから装飾を取り除き、Codeはマスクした文章を作成する
+ */
+const maskCodeNode = (codeNode: TxtTextNode) => {
     return {
-        async [Syntax.Str](node) {
-            const text = getSource(node);
-            const tokens = await tokenize(text);
+        ...codeNode,
+        value: codeNode.value.replace(/./g, "X")
+    };
+};
+const sourceWithoutStyle = (node: TxtParentNode) => {
+    const nodeMaskedCode = {
+        ...node,
+        children: node.children.map((childNode) => {
+            if (childNode.type === "Code") {
+                return maskCodeNode(childNode as TxtTextNode);
+            }
+            return childNode;
+        })
+    };
+    return new StringSource(nodeMaskedCode);
+};
+
+const report: TextlintRuleReporter<Options> = (context) => {
+    const { Syntax, RuleError, report } = context;
+    return {
+        async [Syntax.Paragraph](node) {
+            const source = sourceWithoutStyle(node);
+            const tokens = await tokenize(source.toString());
             tokens.forEach((token) => {
                 if (token.pos === "フィラー") {
+                    const index = token.word_position - 1;
+                    const originalIndex = source.originalIndexFromIndex(index);
                     report(
                         node,
-                        new RuleError(`フィラーである「${token.surface_form}」を検知しました`, {
-                            index: token.word_position - 1
-                        })
+                        new RuleError(
+                            `フィラー（繋ぎ表現）である「${token.surface_form}」を検知しました。
+                        
+「えーと」「あの」「まあ」などの繋ぎ表現は話し言葉（口語）であるため、文章を読みにくくします。`,
+                            {
+                                index: originalIndex
+                            }
+                        )
                     );
                 }
             });
--- test/textlint-rule-no-filler.test.ts
+++ test/textlint-rule-no-filler.test.ts
@@ -2,6 +2,11 @@ import TextLintTester from "textlint-tester";
 import rule from "../src/textlint-rule-no-filler";
 
 const tester = new TextLintTester();
+const errorMessage = (word: string) => {
+    return `フィラー（繋ぎ表現）である「${word}」を検知しました。
+                        
+「えーと」「あの」「まあ」などの繋ぎ表現は話し言葉（口語）であるため、文章を読みにくくします。`;
+};
 tester.run("textlint-rule-no-filler", rule, {
     valid: ["これは問題ない文章です。"],
     invalid: [
@@ -9,7 +14,7 @@ tester.run("textlint-rule-no-filler", rule, {
             text: "えーと、フィラーについてですね。",
             errors: [
                 {
-                    message: "フィラーである「えーと」を検知しました",
+                    message: errorMessage("えーと"),
                     index: 0
                 }
             ]
@@ -18,7 +23,7 @@ tester.run("textlint-rule-no-filler", rule, {
             text: "あのー、この問題について教えれくれますか?",
             errors: [
                 {
-                    message: "フィラーである「あのー」を検知しました",
+                    message: errorMessage("あのー"),
                     index: 0
                 }
             ]
@@ -27,8 +32,17 @@ tester.run("textlint-rule-no-filler", rule, {
             text: "Vueのステート管理ライブラリでああるVuexについての文章。",
             errors: [
                 {
-                    message: "フィラーである「あ」を検知しました",
+                    message: errorMessage("あ"),
                     index: 16
+                }
+            ]
+        },
+        {
+            text: "なんか出た…。",
+            errors: [
+                {
+                    message: errorMessage("なんか"),
+                    index: 0
                 }
             ]
         }
